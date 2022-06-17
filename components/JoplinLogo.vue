1f954265507553ab04f42222016ecf5ce9110c0a
Retr0ve <zhaodacheng@hotmail.com>
2022-06-17 14:44:23 +0800

    feat: initial commit

---
+++ components/JoplinLogo.vue
@@ -0,0 +1,10 @@
+<template>
+  <img class="joplin-logo" alt="Joplin Logo" src="/joplin-logo.png" />
+</template>
+
+<style>
+/* .joplin-logo {
+  height: 180px;
+  width: 180px;
+} */
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app dark>
+    <v-app-bar fixed app>
+      <JoplinLogo class="appbar-logo"/>
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import JoplinLogo from '~/components/JoplinLogo.vue'
+export default {
+  name: 'DefaultLayout',
+  components: { JoplinLogo },
+  data() {
+    return {
+      fixed: false,
+    }
+  },
+}
+</script>
+<style scoped>
+.appbar-logo {
+  height: 40px;
+  object-fit: cover;
+}
+</style>
