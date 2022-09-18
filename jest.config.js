b7f4fe6f0f0bd99e3a9d4d736430ac5525ec7d77
Retrove <zhaodacheng@hotmail.com>
2022-09-18 16:51:12 -0400

    Update jest.config.js
    Co-authored-by: Henry Heino <46334387+personalizedrefrigerator@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
 	preset: "ts-jest",
 	testEnvironment: 'node',
+
+	// Prefer importing from .ts files to importing from .js files
+	moduleFileExtensions: [
+		'ts',
+		'js',
+	],
 	// The glob patterns Jest uses to detect test files
 	testMatch: ['**/*.test.ts'],
 }
