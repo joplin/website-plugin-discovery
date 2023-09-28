ebbe5a967ab45dc68ccdf1977b94e638d7f0807f
Henry Heino <personalizedrefrigerator@gmail.com>
2023-09-28 12:19:55 -0700

    Use markdown-it for markdown rendering

--- build/assets/renderMarkdown.ts
+++ build/assets/renderMarkdown.ts
@@ -1,23 +1,16 @@
-let micromarkRenderer: ((markdown: string) => string) | null = null;
+
+import MarkdownIt from 'markdown-it';
+
+let markdownRenderer: MarkdownIt|null = null;
 
 const renderMarkdown = async (markdown: string) => {
-	// Load micromark
-	if (!micromarkRenderer) {
-		// Micromark only supports ESM.
-		// For ESM-CommonJS interop, we need to use `import`
-		// syntax (which returns promises).
-		const { micromark } = await eval('import("micromark")');
-		const { gfm, gfmHtml } = await eval('import("micromark-extension-gfm")');
-
-		micromarkRenderer = (markdown: string) => {
-			return micromark(markdown, {
-				extensions: [gfm()],
-				htmlExtensions: [gfmHtml()],
-			});
-		};
+	if (!markdownRenderer) {
+		markdownRenderer = new MarkdownIt();
 	}
 
-	return micromarkRenderer(markdown);
+	return markdownRenderer.render(markdown, {
+		linkify: true,
+	});
 };
 
 export default renderMarkdown;
