1828ffc8c8cfad106eb126d34458e251b62116a0
Henry Heino <personalizedrefrigerator@gmail.com>
2024-09-24 11:00:32 -0700

    Add explanation comment

--- src/runtime/util/handleRedirects.ts
+++ src/runtime/util/handleRedirects.ts
@@ -1,5 +1,8 @@
 const handleRedirects = () => {
 	const pageUrl = new URL(location.href);
+
+	// The plugin website was previously hosted at joplin.github.io -- redirect old links
+	// to it's new location:
 	if (pageUrl.host === 'joplin.github.io') {
 		const path = pageUrl.pathname.replace(/^[/]?website-plugin-discovery[/]/, '');
 		location.href = `https://joplinapp.org/plugins/${path}${pageUrl.search}${pageUrl.hash}`;
